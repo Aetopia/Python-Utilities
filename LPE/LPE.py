9fa8569ba5daa199a754791562bcef1fa870af34
Aetopia <41850963+Aetopia@users.noreply.github.com>
2022-01-09 23:46:20 +0530

    Update LPE.py

--- LPE/LPE.py
+++ LPE/LPE.py
@@ -2,7 +2,7 @@ import sys
 import runpy
 import code
 if len(sys.argv) == 1: 
-    code.InteractiveConsole().interact(banner="Lightweight Python Environment: Python 3.9.9\n", exitmsg="")
+    code.InteractiveConsole().interact(banner="Lightweight Python Environment\n", exitmsg="")
 try:
     runpy.run_path(sys.argv[1])
 except:
