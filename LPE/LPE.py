9a3221fce53837cd3481f5371e7efb61332b361e
Aetopia <41850963+Aetopia@users.noreply.github.com>
2022-01-13 22:40:00 +0530

    Update LPE.py

--- LPE/LPE.py
+++ LPE/LPE.py
@@ -1,9 +1,20 @@
 import sys
 import runpy
 import code
+# Flags
+ArgsSupport=False # Enables (sys.argv) & (argparse) Support.
+
+Arguments=sys.argv[2:len(sys.argv)]
+Args={}
+for Index in range(len(Arguments)):
+    Args[Index]=Arguments[Index]
 if len(sys.argv) == 1: 
-    code.InteractiveConsole().interact(banner="Lightweight Python Environment\n", exitmsg="")
-try:
+    code.InteractiveConsole().interact(banner=
+    "Lightweight Python Environment: Python 3.9.9\n", 
+    exitmsg="")
+elif len(sys.argv) == 2:
     runpy.run_path(sys.argv[1])
-except:
-    pass    
+else:
+    if ArgsSupport is True:
+        print("Warning: 'sys.argv' & 'argparse' might likely not work correctly.\n")
+        runpy.run_path(sys.argv[1], Args)
